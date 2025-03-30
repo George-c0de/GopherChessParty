810306f830cb3b923914ea245debd814b24af0d2
George <82450208+George-c0de@users.noreply.github.com>
2025-03-30 23:43:35 +0300

    add ent chess

--- ent/schema/chess.go
+++ ent/schema/chess.go
@@ -1,10 +1,11 @@
 package schema
 
 import (
+	"time"
+
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
 	"github.com/google/uuid"
-	"time"
 )
 
 // Chess holds the schema definition for the Chess entity.
