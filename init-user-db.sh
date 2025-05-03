873ec5f8903fae757937f3241242cd4b8240836d
George <82450208+George-c0de@users.noreply.github.com>
2025-05-04 00:17:16 +0300

    Правильная настройка кредов

--- init-user-db.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Потом создаём роль и базу данных
-psql --username "postgres" <<-EOSQL
-  CREATE ROLE IF NOT EXISTS ${DB_USER} LOGIN PASSWORD '${DB_PASSWORD}';
-  CREATE DATABASE IF NOT EXISTS ${DB_DATABASE} OWNER ${DB_USER};
-EOSQL
